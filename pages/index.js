5849768fe79cbd00c10b82b9222ca964ef52a134
joealsaaidah <alsaaidahyousef@gmail.com>
2021-12-07 13:42:57 +0800

    design the login page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,8 +9,23 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-        <h1>whatsapp-clone</h1>
+      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center bg-[#f8f8f8]'>
+        <div className='flex flex-col items-center justify-center p-24 bg-white rounded-xl shadow-md'>
+          <div className='relative h-24 w-24 mb-10'>
+            <Image
+              src='/images/whsapplogin.png'
+              alt='whatsapp image'
+              layout='fill'
+              objectFit='contain'
+            />
+          </div>
+          <div className='login__text'>
+            <h1 className='text-3xl font-bold'>Sign in to WhatsApp</h1>
+          </div>
+          <button className='mt-12 p-3 rounded-lg bg-[#0a8d48] text-white capitalize '>
+            Sign In With Google
+          </button>
+        </div>
       </main>
     </div>
   );
